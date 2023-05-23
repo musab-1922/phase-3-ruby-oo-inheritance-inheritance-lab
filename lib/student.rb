24dfec79299c036e114d68756e45ba3f18bd0820
musab <musababdi1922@gmail.com>
2023-05-23 08:43:07 +0300

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+# class Student 
+#     attr_accessor :first_name, :last_name
 
-end
+#   def initialize
+#     @first_name = ""
+#     @last_name = ""
+#   end
+# end
+
+class Student < User
+    attr_accessor :knowledge
+  
+    def initialize
+      super
+      @knowledge = []
+    end
+  
+    def learn(string)
+      @knowledge << string
+    end
+  end
